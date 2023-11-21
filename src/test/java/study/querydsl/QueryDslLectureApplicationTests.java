b269490cd40d56924479a0934c5881e99c2ce151
sdfgx123 <sdfgx123@naver.com>
2023-11-21 19:26:24 +0900

    init.

---
+++ src/test/java/study/querydsl/QueryDslLectureApplicationTests.java
@@ -0,0 +1,27 @@
+package study.querydsl;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+
+import javax.persistence.EntityManager;
+
+@SpringBootTest
+@Transactional
+class QueryDslLectureApplicationTests {
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void contextLoads() {
+        Hello hello = new Hello();
+        em.persist(hello);
+
+        JPAQueryFactory query = new JPAQueryFactory(em);
+    }
+
+}
