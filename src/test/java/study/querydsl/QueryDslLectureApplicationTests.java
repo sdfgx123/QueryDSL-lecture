25dcd67ab651853ff80f72bca5f7b4892afc7bcd
sdfgx123 <sdfgx123@naver.com>
2023-11-22 20:16:39 +0900

    feat: QueryDSL 설정 및 검증

--- src/test/java/study/querydsl/QueryDslLectureApplicationTests.java
+++ src/test/java/study/querydsl/QueryDslLectureApplicationTests.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 @Transactional
@@ -22,6 +25,14 @@ class QueryDslLectureApplicationTests {
         em.persist(hello);
 
         JPAQueryFactory query = new JPAQueryFactory(em);
+        QHello qHello = QHello.hello;
+
+        Hello result = query
+                .selectFrom(qHello)
+                .fetchOne();
+
+        assertThat(result).isEqualTo(hello);
+        assertThat(result.getId()).isEqualTo(hello.getId());
     }
 
 }
